1dd254f32f9992a8a2d0b5e68275ed711ad7ecca
magnobrito <magnoleal22@gmail.com>
2024-04-19 06:17:05 -0300

    Writing the problems

---
+++ capitulo_1.py
@@ -0,0 +1,24 @@
+#Exer 5: Crie um programa que peça ao usuário para digitar três números e imprima a soma
+
+
+#Exer 6: Crie um programa que peça ao usuário para digitar um número. Em seguida, o programa deve calcular e mostrar a raiz quadrada desse numero
+
+#Exer 7: Crie um programa que peça ao usuário para digitar um número inteiro. Em seguida, o programa deve calcular e mostrar o valor dos inteiros anterior e posterior a esse número
+
+#Exer 8: Crie um programa que peça ao usuário para digitar um ângulo entre 0 e 360 graus. Em seguida, o programa deve calcular e mostrar o seno, cosseno e a tangente desse número.
+
+#Exer 9: Crie um programa que peça ao usuário para digitar dois números quaisquer. Em seguida, o programa deve calcular e mostrar a potência do primeiro número pelo segundo. 
+
+#Exer 10: Crie um programa que peça ao usuário para digitar três números (A,B e C). Em seguida o programa deve calcular e mostrar os valores das raízes da seguinte equação, usando a fórmula de Bhaskará Ax² + Bx + C = 0
+
+#Exer 11: Crie um programa que peça ao usuário para digitar o raio de um círculo. Em seguida, o programa deve calcular e mostrar a área e o comprimento do círculo.
+
+#Exer 12: Crie um programa que peça ao usuário para digitar as dimensões de um retângulo (largura e altura). Em seguida, o programa deve calcular e mostrar a área e o perímetro desse retângulo.
+
+#Exer 13: Crie um programa que peça ao usuário para digitar a base e a altura de um triângulo. Em seguida, o programa deve calcular e mostrar a área desse triângulo.
+
+#Exer 14: Crie um programa que peça ao usuário para digitar o nome, preço de custo, o preço de venda e a quantidade em estoque de um determinado produto. Em seguida, o programa deve calcular e mostrar o lucro que esse produto pode gerar se todo o estoque for vendido.
+
+#Exer 15: Crie um programa que peça ao usuário para digitar o valor total de uma venda, o percentual de desconto aplicado e o percentual de imposto cobrado. Ao fim, o programa deve mostrar o preço final da mercadoria, sendo que o imposto é cobrado sobre o valor com desconto.
+
+#
