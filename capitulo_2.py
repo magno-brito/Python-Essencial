97df87921208a02b5e11969ad3d08cebdd838758
magnobrito <magnoleal22@gmail.com>
2024-04-21 19:09:26 -0300

    Iniciando capítulo 2

---
+++ capitulo_2.py
@@ -0,0 +1,30 @@
+#Exer 1: Crie um programa que verifique se um número é par ou ímpar
+
+#Exer 2: Crie um programa que verifique se um número digitado pelo usuário é positivo, negativo ou zero.
+
+#Exer 3: Crie um programa que verifique se uma letra digitada pelo usuário é uma vogal ou consoante.
+
+#Exer 4: Crie um programa que verifique se um ano é bissexto ou não.
+
+#Exer 5: Crie um programa que, a partir da idade digitada pelo usuário, verifique se ele é maior de idade ou não.
+
+#Exer 6: Crie um programa que verifique se um texto digitado pelo usuário é um palíndromo ou não (uses texto[::-1] para obter o texto invertido)
+
+#Exer 7: Crie um programa que verifique se um texto digitado pelo usuário é um número inteiro ou não e mostre uma mensagem de acordo (use texto.isdigit() para verificar)
+
+#Exer 8: Crie um programa que verifique se um texto digitado pelo usuário corresponde a uma data no formato "dd/mm/aaaa", considerando que a data deve ter 10 caracteres, "dd" deve variar de 01 a 31, "mm" de 01 a 12 e "aaaa" de 0001 a 9999. Utilize colchetes para acessar os caracteres do texto pelo seu índice (exemplos: texto[0:2] )
+
+#Exer 9: Crie um programa que calcule a média de três números e exiba a mensagem "Aprovado" se for maior ou igual a 6 ou "Reprobado" caso contrário. Se a nota for 10 exiba parabéns. 
+
+#Exer 10: Crie um programa que verifique se uma temperatura corporal digitada pelo usuário está acima, abaixou ou dentro da faixa normal (36°C a 37°C)
+
+#Exer 11: Crie um programa que verifique se uma pessoa pode votar ou não, considerando sua idade e sua nacionalidade, digitadas pelo usuário (se tem 16 anos ou mais e é brasileiro)
+
+#Exer 12: Crie um programa que verifique se uma pessoa é elegível para aposentadoria (se tem 60 anos ou mais para mulheres ou 65 anos para homens). O usuário deve digitar a idade e o gênero da pessoa a ser verificada.
+
+#Exer 13: Crie um programa que verifique se um número inteiro digitado pelo usuário é divisível por outro número inteiro também digitado pelo usuário.
+
+#Exer 14: Crie um programa que pergunte seu salário e exiba uma mensagem "Alto Salário" se o salário for maior que R$ 10.000,00 ou "Baixo Salário" caso contrátio.
+
+#Exer 15: Crie um programa que peça ao usuário para digitar uma palavra. O programa deve então indicar se a palavra inserida começa com uma vogal ou com um consoante. 
+
